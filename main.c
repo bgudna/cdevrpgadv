92ab1e7be1e96bac749ba57fd788e5be849746f4
bgudna <b@bgudna.com>
2020-05-02 13:03:44 +0200

    basic input added with minor keyword-verb matching

--- main.c
+++ main.c
@@ -1,10 +1,37 @@
 #include<stdio.h>
+#include<string.h>
+
+static char input[100];
+
+static int getMeSomeInput() {
+    printf("\n-->");
+    return fgets(input, sizeof(input), stdin) != NULL;
+}
+
+static int getAndParseAndGo() {
+    char *verb = strtok(input, "\n");
+    char *noun = strtok(NULL, "\n");
+
+    if (verb != NULL) {
+        if(strcmp(verb, "quit") == 0) {
+            return 0;
+        } else if (strcmp(verb, "look") == 0) {
+            printf("You see nothing, your eyes are bleeding\n");
+        } else if (strcmp(verb, "go") == 0) {
+            printf("You can't go anywhere, your eyes are bleeding from the encounter\n");
+        } else {
+            printf("I can't really '%s' right now\n", verb);
+        }
+    }
+    return 1;
+}
 
 int main() {
-    printf("This is the intro text saying hello\n");
-    printf("Something else to be said about this\n");
-    printf("w00t!");
+    printf("Welcome to the screaming brain.\n");
+    printf("Something else to be said about this...\n");
     
-   
-       return 0;
+    while (getMeSomeInput() && getAndParseAndGo());
+    
+    printf("alrighty then!\n");
+    return 0;
 }
