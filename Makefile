929c20b958156873179adcc3ea017824f32a346e
bgudna <b@bgudna.com>
2020-05-13 21:53:32 +0200

    location cleanup and objects setup

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 # basic makefile for later updating
 
 all: 
-	gcc main.c places.c places.h -o build/main
+	gcc main.c places.c places.h hlutur.c hlutur.h ymislegt.c ymislegt.h -o build/main
--- places.c
+++ places.c
@@ -1,41 +1,32 @@
 #include<stdio.h>
 #include<string.h>
-
-struct places {
-    const char *lysing;
-    const char *tagg;
-}
-
-stadir[2] = {
-    {"the vast expanse", "expanse"},
-    {"an empty dog house", "dog house"}
-};
-
-#define fjoldiStada (sizeof(stadir) / sizeof(*stadir))
-
-static unsigned whereIsMe = 0;
+#include "hlutur.h"
+#include "ymislegt.h"
 
 void goLook(const char *noun){
     if (noun != NULL && strcmp(noun, "around") == 0) {
-        printf("You be at %s\n", stadir[whereIsMe].lysing);
+        printf("You be at %s\n", player->stadur->lysing);
+        listaHlutiStadsetningar(player->stadur);
     } else {
         printf("You are lost and confused.\n");
     }
 }
 
 void goGo(const char *noun){
-    unsigned i;
-    for (i = 0; i <  fjoldiStada; i++) {
-        if (noun != NULL && strcmp(noun, stadir[i].tagg) == 0) {
-            if (i == whereIsMe) {
-                printf("useless, this is where you are now.\n");
-            } else {
-                printf("alright.\n");
-                whereIsMe = i;
-                goLook("around");
-            }
-            return;
-        }
-    }
-    printf("there is mystery in your requests.\n");
+    
+    HLUTUR *hlutur = parsaHlut(noun);
+   if (hlutur == NULL)
+   {
+      printf("there is mystery in your requests.\n");
+   }
+   else if (hlutur == player->stadur)
+   {
+      printf("useless, this is where you are now.\n");
+   }
+   else
+   {
+      printf("granted.\n");
+      player->stadur = hlutur;
+      goLook("around");
+   }
 }
